4d3a08c5de6fe42514f4ac40cb0dfa0e02ac9de3
Michael de Radobýl <mj-programming@gmx.de>
2026-02-05 13:36:46 +0100

    Rebase the source code

---
+++ BitLogicCalculator/StatisticsForm.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace BitLogicCalculator
+{
+	partial class StatisticsForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			tableLayoutPanel = new TableLayoutPanel();
+			labelA2Bit0Number = new Label();
+			labelA1Bit0Number = new Label();
+			labelTotal = new Label();
+			labelBit0 = new Label();
+			labelResult = new Label();
+			labelAccumulator2 = new Label();
+			labelAccumulator1 = new Label();
+			labelBit1 = new Label();
+			labelResultBit0Number = new Label();
+			labelA1Bit1Number = new Label();
+			labelA2Bit1Number = new Label();
+			labelResultBit1Number = new Label();
+			labelA1TotalNumber = new Label();
+			labelA2TotalNumber = new Label();
+			labelResultTotalNumber = new Label();
+			toolTip = new ToolTip(components);
+			statusStrip = new StatusStrip();
+			toolStripStatusLabel = new ToolStripStatusLabel();
+			toolStripContainer1 = new ToolStripContainer();
+			tableLayoutPanel.SuspendLayout();
+			statusStrip.SuspendLayout();
+			toolStripContainer1.BottomToolStripPanel.SuspendLayout();
+			toolStripContainer1.ContentPanel.SuspendLayout();
+			toolStripContainer1.SuspendLayout();
+			SuspendLayout();
+			// 
+			// tableLayoutPanel
+			// 
+			tableLayoutPanel.BackColor = Color.Transparent;
+			tableLayoutPanel.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
+			tableLayoutPanel.ColumnCount = 4;
+			tableLayoutPanel.ColumnStyles.Add(new ColumnStyle());
+			tableLayoutPanel.ColumnStyles.Add(new ColumnStyle());
+			tableLayoutPanel.ColumnStyles.Add(new ColumnStyle());
+			tableLayoutPanel.ColumnStyles.Add(new ColumnStyle());
+			tableLayoutPanel.Controls.Add(labelA2Bit0Number, 2, 1);
+			tableLayoutPanel.Controls.Add(labelA1Bit0Number, 1, 1);
+			tableLayoutPanel.Controls.Add(labelTotal, 0, 3);
+			tableLayoutPanel.Controls.Add(labelBit0, 0, 1);
+			tableLayoutPanel.Controls.Add(labelResult, 3, 0);
+			tableLayoutPanel.Controls.Add(labelAccumulator2, 2, 0);
+			tableLayoutPanel.Controls.Add(labelAccumulator1, 1, 0);
+			tableLayoutPanel.Controls.Add(labelBit1, 0, 2);
+			tableLayoutPanel.Controls.Add(labelResultBit0Number, 3, 1);
+			tableLayoutPanel.Controls.Add(labelA1Bit1Number, 1, 2);
+			tableLayoutPanel.Controls.Add(labelA2Bit1Number, 2, 2);
+			tableLayoutPanel.Controls.Add(labelResultBit1Number, 3, 2);
+			tableLayoutPanel.Controls.Add(labelA1TotalNumber, 1, 3);
+			tableLayoutPanel.Controls.Add(labelA2TotalNumber, 2, 3);
+			tableLayoutPanel.Controls.Add(labelResultTotalNumber, 3, 3);
+			tableLayoutPanel.Dock = DockStyle.Fill;
+			tableLayoutPanel.Location = new Point(0, 0);
+			tableLayoutPanel.Margin = new Padding(4, 3, 4, 3);
+			tableLayoutPanel.Name = "tableLayoutPanel";
+			tableLayoutPanel.RowCount = 4;
+			tableLayoutPanel.RowStyles.Add(new RowStyle());
+			tableLayoutPanel.RowStyles.Add(new RowStyle());
+			tableLayoutPanel.RowStyles.Add(new RowStyle());
+			tableLayoutPanel.RowStyles.Add(new RowStyle());
+			tableLayoutPanel.Size = new Size(354, 71);
+			tableLayoutPanel.TabIndex = 0;
+			// 
+			// labelA2Bit0Number
+			// 
+			labelA2Bit0Number.AutoSize = true;
+			labelA2Bit0Number.BackColor = Color.Transparent;
+			labelA2Bit0Number.Dock = DockStyle.Fill;
+			labelA2Bit0Number.Location = new Point(146, 19);
+			labelA2Bit0Number.Margin = new Padding(4, 0, 4, 0);
+			labelA2Bit0Number.Name = "labelA2Bit0Number";
+			labelA2Bit0Number.Size = new Size(85, 15);
+			labelA2Bit0Number.TabIndex = 4;
+			labelA2Bit0Number.Text = "---";
+			labelA2Bit0Number.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// labelA1Bit0Number
+			// 
+			labelA1Bit0Number.AutoSize = true;
+			labelA1Bit0Number.BackColor = Color.Transparent;
+			labelA1Bit0Number.Dock = DockStyle.Fill;
+			labelA1Bit0Number.Location = new Point(51, 19);
+			labelA1Bit0Number.Margin = new Padding(4, 0, 4, 0);
+			labelA1Bit0Number.Name = "labelA1Bit0Number";
+			labelA1Bit0Number.Size = new Size(85, 15);
+			labelA1Bit0Number.TabIndex = 3;
+			labelA1Bit0Number.Text = "---";
+			labelA1Bit0Number.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// labelTotal
+			// 
+			labelTotal.AutoSize = true;
+			labelTotal.BackColor = Color.Transparent;
+			labelTotal.Dock = DockStyle.Fill;
+			labelTotal.Location = new Point(6, 53);
+			labelTotal.Margin = new Padding(4, 0, 4, 0);
+			labelTotal.Name = "labelTotal";
+			labelTotal.Size = new Size(35, 16);
+			labelTotal.TabIndex = 5;
+			labelTotal.Text = "Total:";
+			labelTotal.TextAlign = ContentAlignment.MiddleLeft;
+			// 
+			// labelBit0
+			// 
+			labelBit0.AutoSize = true;
+			labelBit0.BackColor = Color.Transparent;
+			labelBit0.Dock = DockStyle.Fill;
+			labelBit0.Location = new Point(6, 19);
+			labelBit0.Margin = new Padding(4, 0, 4, 0);
+			labelBit0.Name = "labelBit0";
+			labelBit0.Size = new Size(35, 15);
+			labelBit0.TabIndex = 2;
+			labelBit0.Text = "Bit 0:";
+			labelBit0.TextAlign = ContentAlignment.MiddleLeft;
+			// 
+			// labelResult
+			// 
+			labelResult.AutoSize = true;
+			labelResult.BackColor = Color.Transparent;
+			labelResult.Dock = DockStyle.Fill;
+			labelResult.Location = new Point(241, 2);
+			labelResult.Margin = new Padding(4, 0, 4, 0);
+			labelResult.Name = "labelResult";
+			labelResult.Size = new Size(107, 15);
+			labelResult.TabIndex = 3;
+			labelResult.Text = "Result";
+			labelResult.TextAlign = ContentAlignment.TopCenter;
+			// 
+			// labelAccumulator2
+			// 
+			labelAccumulator2.AutoSize = true;
+			labelAccumulator2.BackColor = Color.Transparent;
+			labelAccumulator2.Dock = DockStyle.Fill;
+			labelAccumulator2.Location = new Point(146, 2);
+			labelAccumulator2.Margin = new Padding(4, 0, 4, 0);
+			labelAccumulator2.Name = "labelAccumulator2";
+			labelAccumulator2.Size = new Size(85, 15);
+			labelAccumulator2.TabIndex = 3;
+			labelAccumulator2.Text = "Accumulator 2";
+			labelAccumulator2.TextAlign = ContentAlignment.TopCenter;
+			// 
+			// labelAccumulator1
+			// 
+			labelAccumulator1.AutoSize = true;
+			labelAccumulator1.BackColor = Color.Transparent;
+			labelAccumulator1.Dock = DockStyle.Fill;
+			labelAccumulator1.Location = new Point(51, 2);
+			labelAccumulator1.Margin = new Padding(4, 0, 4, 0);
+			labelAccumulator1.Name = "labelAccumulator1";
+			labelAccumulator1.Size = new Size(85, 15);
+			labelAccumulator1.TabIndex = 3;
+			labelAccumulator1.Text = "Accumulator 1";
+			labelAccumulator1.TextAlign = ContentAlignment.TopCenter;
+			// 
+			// labelBit1
+			// 
+			labelBit1.AutoSize = true;
+			labelBit1.BackColor = Color.Transparent;
+			labelBit1.Dock = DockStyle.Fill;
+			labelBit1.Location = new Point(6, 36);
+			labelBit1.Margin = new Padding(4, 0, 4, 0);
+			labelBit1.Name = "labelBit1";
+			labelBit1.Size = new Size(35, 15);
+			labelBit1.TabIndex = 4;
+			labelBit1.Text = "Bit 1:";
+			labelBit1.TextAlign = ContentAlignment.MiddleLeft;
+			// 
+			// labelResultBit0Number
+			// 
+			labelResultBit0Number.AutoSize = true;
+			labelResultBit0Number.BackColor = Color.Transparent;
+			labelResultBit0Number.Dock = DockStyle.Fill;
+			labelResultBit0Number.Location = new Point(241, 19);
+			labelResultBit0Number.Margin = new Padding(4, 0, 4, 0);
+			labelResultBit0Number.Name = "labelResultBit0Number";
+			labelResultBit0Number.Size = new Size(107, 15);
+			labelResultBit0Number.TabIndex = 6;
+			labelResultBit0Number.Text = "---";
+			labelResultBit0Number.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// labelA1Bit1Number
+			// 
+			labelA1Bit1Number.AutoSize = true;
+			labelA1Bit1Number.BackColor = Color.Transparent;
+			labelA1Bit1Number.Dock = DockStyle.Fill;
+			labelA1Bit1Number.Location = new Point(51, 36);
+			labelA1Bit1Number.Margin = new Padding(4, 0, 4, 0);
+			labelA1Bit1Number.Name = "labelA1Bit1Number";
+			labelA1Bit1Number.Size = new Size(85, 15);
+			labelA1Bit1Number.TabIndex = 7;
+			labelA1Bit1Number.Text = "---";
+			labelA1Bit1Number.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// labelA2Bit1Number
+			// 
+			labelA2Bit1Number.AutoSize = true;
+			labelA2Bit1Number.BackColor = Color.Transparent;
+			labelA2Bit1Number.Dock = DockStyle.Fill;
+			labelA2Bit1Number.Location = new Point(146, 36);
+			labelA2Bit1Number.Margin = new Padding(4, 0, 4, 0);
+			labelA2Bit1Number.Name = "labelA2Bit1Number";
+			labelA2Bit1Number.Size = new Size(85, 15);
+			labelA2Bit1Number.TabIndex = 8;
+			labelA2Bit1Number.Text = "---";
+			labelA2Bit1Number.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// labelResultBit1Number
+			// 
+			labelResultBit1Number.AutoSize = true;
+			labelResultBit1Number.BackColor = Color.Transparent;
+			labelResultBit1Number.Dock = DockStyle.Fill;
+			labelResultBit1Number.Location = new Point(241, 36);
+			labelResultBit1Number.Margin = new Padding(4, 0, 4, 0);
+			labelResultBit1Number.Name = "labelResultBit1Number";
+			labelResultBit1Number.Size = new Size(107, 15);
+			labelResultBit1Number.TabIndex = 9;
+			labelResultBit1Number.Text = "---";
+			labelResultBit1Number.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// labelA1TotalNumber
+			// 
+			labelA1TotalNumber.AutoSize = true;
+			labelA1TotalNumber.BackColor = Color.Transparent;
+			labelA1TotalNumber.Dock = DockStyle.Fill;
+			labelA1TotalNumber.Location = new Point(51, 53);
+			labelA1TotalNumber.Margin = new Padding(4, 0, 4, 0);
+			labelA1TotalNumber.Name = "labelA1TotalNumber";
+			labelA1TotalNumber.Size = new Size(85, 16);
+			labelA1TotalNumber.TabIndex = 10;
+			labelA1TotalNumber.Text = "---";
+			labelA1TotalNumber.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// labelA2TotalNumber
+			// 
+			labelA2TotalNumber.AutoSize = true;
+			labelA2TotalNumber.BackColor = Color.Transparent;
+			labelA2TotalNumber.Dock = DockStyle.Fill;
+			labelA2TotalNumber.Location = new Point(146, 53);
+			labelA2TotalNumber.Margin = new Padding(4, 0, 4, 0);
+			labelA2TotalNumber.Name = "labelA2TotalNumber";
+			labelA2TotalNumber.Size = new Size(85, 16);
+			labelA2TotalNumber.TabIndex = 11;
+			labelA2TotalNumber.Text = "---";
+			labelA2TotalNumber.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// labelResultTotalNumber
+			// 
+			labelResultTotalNumber.AutoSize = true;
+			labelResultTotalNumber.BackColor = Color.Transparent;
+			labelResultTotalNumber.Dock = DockStyle.Fill;
+			labelResultTotalNumber.Location = new Point(241, 53);
+			labelResultTotalNumber.Margin = new Padding(4, 0, 4, 0);
+			labelResultTotalNumber.Name = "labelResultTotalNumber";
+			labelResultTotalNumber.Size = new Size(107, 16);
+			labelResultTotalNumber.TabIndex = 12;
+			labelResultTotalNumber.Text = "---";
+			labelResultTotalNumber.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// statusStrip
+			// 
+			statusStrip.Dock = DockStyle.None;
+			statusStrip.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel });
+			statusStrip.Location = new Point(0, 0);
+			statusStrip.Name = "statusStrip";
+			statusStrip.ShowItemToolTips = true;
+			statusStrip.Size = new Size(354, 22);
+			statusStrip.SizingGrip = false;
+			statusStrip.TabIndex = 1;
+			statusStrip.TabStop = true;
+			statusStrip.Text = "statusStrip";
+			// 
+			// toolStripStatusLabel
+			// 
+			toolStripStatusLabel.Name = "toolStripStatusLabel";
+			toolStripStatusLabel.Size = new Size(93, 17);
+			toolStripStatusLabel.Text = "information text";
+			// 
+			// toolStripContainer1
+			// 
+			// 
+			// toolStripContainer1.BottomToolStripPanel
+			// 
+			toolStripContainer1.BottomToolStripPanel.Controls.Add(statusStrip);
+			// 
+			// toolStripContainer1.ContentPanel
+			// 
+			toolStripContainer1.ContentPanel.AutoScroll = true;
+			toolStripContainer1.ContentPanel.Controls.Add(tableLayoutPanel);
+			toolStripContainer1.ContentPanel.Margin = new Padding(4, 3, 4, 3);
+			toolStripContainer1.ContentPanel.Size = new Size(354, 71);
+			toolStripContainer1.Dock = DockStyle.Fill;
+			toolStripContainer1.Location = new Point(0, 0);
+			toolStripContainer1.Margin = new Padding(4, 3, 4, 3);
+			toolStripContainer1.Name = "toolStripContainer1";
+			toolStripContainer1.Size = new Size(354, 93);
+			toolStripContainer1.TabIndex = 2;
+			toolStripContainer1.Text = "toolStripContainer1";
+			toolStripContainer1.TopToolStripPanelVisible = false;
+			// 
+			// StatisticsForm
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(354, 93);
+			Controls.Add(toolStripContainer1);
+			FormBorderStyle = FormBorderStyle.FixedToolWindow;
+			Margin = new Padding(4, 3, 4, 3);
+			Name = "StatisticsForm";
+			ShowInTaskbar = false;
+			StartPosition = FormStartPosition.CenterParent;
+			Text = "Statistics";
+			Load += StatisticsForm_Load;
+			tableLayoutPanel.ResumeLayout(false);
+			tableLayoutPanel.PerformLayout();
+			statusStrip.ResumeLayout(false);
+			statusStrip.PerformLayout();
+			toolStripContainer1.BottomToolStripPanel.ResumeLayout(false);
+			toolStripContainer1.BottomToolStripPanel.PerformLayout();
+			toolStripContainer1.ContentPanel.ResumeLayout(false);
+			toolStripContainer1.ResumeLayout(false);
+			toolStripContainer1.PerformLayout();
+			ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+		private System.Windows.Forms.ToolTip toolTip;
+		private System.Windows.Forms.StatusStrip statusStrip;
+		private System.Windows.Forms.Label labelA2Bit0Number;
+		private System.Windows.Forms.Label labelA1Bit0Number;
+		private System.Windows.Forms.Label labelTotal;
+		private System.Windows.Forms.Label labelBit0;
+		private System.Windows.Forms.Label labelResult;
+		private System.Windows.Forms.Label labelAccumulator2;
+		private System.Windows.Forms.Label labelAccumulator1;
+		private System.Windows.Forms.Label labelBit1;
+		private System.Windows.Forms.Label labelResultBit0Number;
+		private System.Windows.Forms.Label labelA1Bit1Number;
+		private System.Windows.Forms.Label labelA2Bit1Number;
+		private System.Windows.Forms.Label labelResultBit1Number;
+		private System.Windows.Forms.Label labelA1TotalNumber;
+		private System.Windows.Forms.Label labelA2TotalNumber;
+		private System.Windows.Forms.Label labelResultTotalNumber;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+		private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+	}
+}
